c42a264b47656bb507004b2ab26372abb330b43b
sayvalery <vsayfullin@icloud.com>
2025-02-02 05:34:12 +0300

    fixed animation and headerxs

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,7 +6,7 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function formatDate(date: Date) {
-  return Intl.DateTimeFormat("en-US", {
+  return Intl.DateTimeFormat("ru-RU", {
     month: "short",
     day: "2-digit",
     year: "numeric"
